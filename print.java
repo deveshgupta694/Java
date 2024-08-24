1d0402293bf971e79434323d62d45f11f84db916
deveshgupta694 <113123529+deveshgupta694@users.noreply.github.com>
2024-08-24 11:43:58 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,14 @@
+class print{
+    public static void main(String[] args) {
+        int x = 35;
+        String str = "Devesh Gupta";
+        float cgpa = 7.9f;
+        double sgpa = 8.1;
+        char grade = 'B';
+        boolean vote = true;
+        System.out.println(str+" ka roll no hay : "+x+" uska cgpa hay "+cgpa+" and grade is "+grade+" total spga "+sgpa);
+
+        if(vote)System.out.println("is eligible to vote");
+        else System.out.println("baday ho jao");
+    }
+}
